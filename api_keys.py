7edf1ea9311a419c936520b2fb15f8970b9a65ce
Caleb Meinke <cameinke@gmail.com>
2024-12-06 22:20:37 -0600

    Updated 12-06

--- api_keys.py
+++ api_keys.py
@@ -1,8 +1,5 @@
-# OpenWeatherMap API Key
-weather_api_key = "814fcea7067d60a6799516103afde4a6"
-
 # Geoapify API Key
-geoapify_key = "b49a48745a5544e58d95425483135592"
+geoapify_key = "YOUR API KEY HERE"
 
 # OpenCage Reverse Geocoding API
-opencage_key = "1b862c5a45cb438fb60184e01b394549"
+opencage_key = "YOUR API KEY HERE"
